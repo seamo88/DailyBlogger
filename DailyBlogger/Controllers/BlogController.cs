33bc85b07bea9a0a28a82ff72df2168c38887d03
seamo88 <bstockgame.88@gmail.com>
2020-05-05 14:03:12 -0500

    Add project files.

---
+++ DailyBlogger/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DailyBlogger.Models;
+
+namespace DailyBlogger.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogContext _context;
+
+        public BlogController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult list()
+        {
+            IEnumerable<BlogPost> posts = _context.blogPost.ToList<BlogPost>();
+            return View();
+        }
+    }
+}
